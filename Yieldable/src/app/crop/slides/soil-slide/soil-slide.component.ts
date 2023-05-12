8aa1b235e635ce87062d06158a3b886afb1d24e0
karl-resultan <59629898+Jagaimo2@users.noreply.github.com>
2023-05-12 16:56:08 +0800

    Major update
    - Soil Analysis Based calculations now functional
    - Added soil analysis results form in soil-type template

--- Yieldable/src/app/crop/slides/soil-slide/soil-slide.component.ts
+++ Yieldable/src/app/crop/slides/soil-slide/soil-slide.component.ts
@@ -27,7 +27,8 @@ export class SoilSlideComponent implements OnInit {
     this.getMaxVals();
   }
 
-  updateValues(soilType: string){
+  updateValues(){
+    //soil type
     let clay_amount = parseInt((<HTMLInputElement>document.getElementById('soil-1')).value);
     let silt_amount = parseInt((<HTMLInputElement>document.getElementById('soil-2')).value);
     let sand_amount = parseInt((<HTMLInputElement>document.getElementById('soil-3')).value);
@@ -40,8 +41,35 @@ export class SoilSlideComponent implements OnInit {
     let diff2 = this.getDiff(this.crop.optimal_silt, silt_amount);
     let diff3 = this.getDiff(this.crop.optimal_sand, sand_amount);
 
-    let calculation = this.crop.crop_yield - (this.crop.crop_yield * (((diff1) + (diff2) + (diff3)) / 100));
+    // macro nutrients
+    let nitrogen = parseInt((<HTMLInputElement>document.getElementById('nitrogen')).value);
+    let potassium = parseInt((<HTMLInputElement>document.getElementById('potassium')).value);
+    let phosphorus = parseInt((<HTMLInputElement>document.getElementById('phosphorus')).value);
 
+    // nutrient deficiency yield reduction calculation
+    let yield_per_macronutrient = .3333 * this.crop.crop_yield;
+    let reduction_per_nitrogen_deficiency = yield_per_macronutrient / 4.4;
+    let reduction_per_potassium_deficiency = yield_per_macronutrient / 249.9;
+    let reduction_per_phosphorus_deficiency = yield_per_macronutrient / 29.9;
+
+    let nitrogen_deficit_yield, potassium_deficit_yield, phosphorus_deficit_yield = yield_per_macronutrient;
+
+    if (nitrogen < 4.5 && nitrogen > 0) {
+      nitrogen_deficit_yield = yield_per_macronutrient - ((4.4 - nitrogen) * reduction_per_nitrogen_deficiency);
+    }
+    if (potassium < 250 && potassium > 0) {
+      potassium_deficit_yield = yield_per_macronutrient - ((249.9 - potassium) * reduction_per_potassium_deficiency);
+    }
+    if (phosphorus < 30 && phosphorus > 0) {
+      phosphorus_deficit_yield = yield_per_macronutrient - ((29.9 - phosphorus) * reduction_per_phosphorus_deficiency);
+    }
+
+    let calculation = nitrogen_deficit_yield + potassium_deficit_yield + phosphorus_deficit_yield;
+
+    // soil type yield reduction calculation
+    calculation -= (calculation * (((diff1) + (diff2) + (diff3)) / 100));
+
+    // prevent negative values
     if (calculation < 0){
       calculation = 0;
     }
